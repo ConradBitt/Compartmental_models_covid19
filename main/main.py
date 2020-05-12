e56b998270f3e2673f8df65536bc0475e0a895f9
Conrado <conrad.bittencourt@gmail.com>
2020-05-12 18:51:58 -0300

    Commit do arquivo principal que vai chamar a classe para simular.

---
+++ main/main.py
@@ -0,0 +1,18 @@
+from models import Doenca
+# Levando em conta os dados da COVID-19
+
+c = 3  # contato entre as pessoas
+p = 0.10  # probabilidade de contaminar
+beta = c * p
+tempo_de_contaminacao = 4  # o tempo de residência no compartimento infeccioso, isto é, o
+# tempo médio em que um indivíduo pode infectar outras pessoas.
+# Para a gripe, o período infecioso é tipicamente de 1 a 3 dias
+gama = 1 / tempo_de_contaminacao  # coeficiente de recuperação.
+
+mortalidade = 0.04
+
+corona = Doenca(populacao=900000, beta=beta, gama=gama,
+                tDeath=mortalidade, infectado=5100, simulationTime=360, leitos=5623)
+
+corona.run()
+corona.plot(mortalidade, c, p)
